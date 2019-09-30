1236822cdb1fc2742bf7d1a8d2cc33267729baca
Jason Chen <jason.sl.chen@gmail.com>
2019-09-29 23:35:27 -0700

    made basic form for submitting reviews

--- src/client/desserts/dessert.jsx
+++ src/client/desserts/dessert.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import axios from 'axios';
+import ReviewForm from '../reviewForm.jsx';
 
 const Title = styled.div`
   border: 1px solid black;
@@ -9,17 +10,24 @@ const Title = styled.div`
 
 const Dessert = (props) => {
   const dessertName = props.match.params.dessertId;
+  console.log(props.match);
   const foodTitle = dessertName.split('-').map((word) => {
     const capitalize = word.split('');
     capitalize[0] = capitalize[0].toUpperCase();
     return capitalize.join('');
   }).join(' ');
+  const images = [];
+
+  useEffect(() => {
+    axios.get('');
+  });
 
   return (
     <div>
       <Title>
         {foodTitle}
       </Title>
+      <ReviewForm />
     </div>
   );
 };
---
+++ src/client/reviewForm.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NumberInput = styled.input`
+::-webkit-inner-spin-button{
+  -webkit-appearance: none; 
+  margin: 0; 
+}
+::-webkit-outer-spin-button{
+  -webkit-appearance: none; 
+  margin: 0; 
+}  
+`;
+
+const RatingSelect = styled.select`
+-webkit-appearance: none;
+-webkit-border-radius: 0px;
+width: 25%
+`;
+
+const Form = styled.form``;
+
+const ReviewForm = (props) => (
+  <Form>
+    <div>
+      <label>
+            Name:
+      </label>
+      <div>
+        <input type="text" id="reviewer" />
+      </div>
+    </div>
+    <div>
+      <label>
+            Rating:
+      </label>
+      <RatingSelect id="rating">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </RatingSelect>
+    </div>
+    <div>
+      <label>Cost:</label>
+      <div>
+        <NumberInput min="0" id="cost" type="number" />
+      </div>
+    </div>
+    <div>
+      <label>
+            Date:
+      </label>
+      <div>
+        <input type="date" id="date" />
+      </div>
+    </div>
+    <div>
+      <label>
+            Restaurant:
+      </label>
+      <div>
+        <input type="text" id="restaurant" />
+      </div>
+    </div>
+    <div>
+      <label>
+            Review:
+      </label>
+      <div>
+        <textarea rows="5" cols="40" id="review" maxLength="280" />
+      </div>
+    </div>
+    <div>
+      <input type="submit" value="Submit Review!" />
+    </div>
+  </Form>
+);
+
+export default ReviewForm;
